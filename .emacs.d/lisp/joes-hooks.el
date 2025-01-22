36b5a66482844ec76b3778b02b67d36b47e1c8a7
Joe Kohler <joe.fbs.junior@gmail.com>
2025-01-22 10:57:40 -0300

    Fixed identation and added fill-column to better deal with 1080 res and 160pt font height

--- .emacs.d/lisp/joes-hooks.el
+++ .emacs.d/lisp/joes-hooks.el
@@ -35,16 +35,17 @@
 	(display-line-numbers-mode 1)
 	(setq tab-width 4)
 	(setq-local company-dabbrev-ignore-case nil)
-    (setq-local company-dabbrev-downcase nil))
+	(setq-local company-dabbrev-downcase nil))
 
 (defun my-text-mode-hook ()
-    (buffer-face-mode))
+	(buffer-face-mode))
 
 (defun my-latex-mode-hook ()
 	(local-set-key [remap tex-compile] 'tex-compile-update)
 	(add-to-list 'company-capf-prefix-functions 'my-latex-company-capf-prefix)
 	(eglot-ensure)
 	(flymake-mode 1)
+	(set-fill-column 63)
 	(auto-fill-mode 1))
 
 (defun my-save-hook ()
@@ -52,9 +53,9 @@
 
 (defun my-python-mode-hook ()
 	(setq indent-tabs-mode nil)
-    (highlight-indent-guides-mode t)
+	(highlight-indent-guides-mode t)
 	(pet-mode)
-    (set-python-keybindings)
+	(set-python-keybindings)
 	(eglot-ensure))
 
 (defun my-lsp-hook ()
@@ -63,33 +64,33 @@
 
 (defun my-tree-sitter-mode-hook()
 	(require 'tree-sitter-indent)
-    (add-to-list 'company-capf-prefix-functions 'my-tree-sitter-company-capf-prefix)
-    (tree-sitter-hl-mode)
+	(add-to-list 'company-capf-prefix-functions 'my-tree-sitter-company-capf-prefix)
+	(tree-sitter-hl-mode)
 	(when (boundp
 			  (intern (format "tree-sitter-indent-%s-scopes"
 						  (replace-regexp-in-string (rx "-mode") "" (symbol-name major-mode)))))
 		(tree-sitter-indent-mode)))
 
 (defun my-cpp-mode-hook ()
-    (eglot-ensure))
+	(eglot-ensure))
 
 (defun my-csharp-mode-hook ()
-  (require 'whitespace)
-  (require 'tree-sitter-langs)
-  (setq-local csharp-indent-offset (my-buffer-indentation-offset))
-  (setenv "FrameworkPathOverride" framework-path-override)
-  (eglot-ensure)
+	(require 'whitespace)
+	(require 'tree-sitter-langs)
+	(setq-local csharp-indent-offset (my-buffer-indentation-offset))
+	(setenv "FrameworkPathOverride" framework-path-override)
+	(eglot-ensure)
 
-  (tree-sitter-hl-add-patterns 'c-sharp
-    [(variable_declarator (identifier) @variable.parameter)])
+	(tree-sitter-hl-add-patterns 'c-sharp
+		[(variable_declarator (identifier) @variable.parameter)])
 
-  (setq whitespace-style '(face trailing space-before-tab empty space-after-tab tab-mark))
-  (whitespace-mode 1)
-  (eldoc-mode -1))
+	(setq whitespace-style '(face trailing space-before-tab empty space-after-tab tab-mark))
+	(whitespace-mode 1)
+	(eldoc-mode -1))
 
 (defun my-git-commit-mode-hook ()
 	(setq-local company-dabbrev-ignore-case nil)
-    (setq-local company-dabbrev-downcase nil)
+	(setq-local company-dabbrev-downcase nil)
 	(set-git-commit-keybindings))
 
 (provide 'joes-hooks)
