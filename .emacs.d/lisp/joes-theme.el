2655ab1fa5340b34345a35bcbdcee7da1fe74f05
Joe Kohler <joe.fbs.junior@gmail.com>
2025-02-07 15:49:13 -0300

    minor theme updating. Making modeline a bit darker.

--- .emacs.d/lisp/joes-theme.el
+++ .emacs.d/lisp/joes-theme.el
@@ -26,17 +26,10 @@
 (defface documents-face '((t :font "Iosevka Etoile"))
 	"Face for text documents.")
 
-(defun joes-theme-apply-flymake()
-	(set-face-attribute 'flymake-error nil :underline '(:color "#F00" :style wave))
-	(set-face-attribute 'flymake-warning nil :underline '(:color "#FF0" :style wave))
-	(set-face-attribute 'flymake-note nil :underline '(:color "#00F" :style wave))
-	(setq-default flymake-note-bitmap '(right-arrow compilation-note))
-	(setq-default flymake-warning-bitmap '(right-triangle compilation-warning))
-	(setq-default flymake-error-bitmap '(flymake-double-exclamation-mark compilation-error)))
-
 (defun joes-theme-apply-default-faces ()
+	"Default face."
 	(setq-default visual-line-fringe-indicators 'left-curly-arrow right-curly-arrow)
-	(setq-default buffer-face-mode-face 'documents-face)
+	(setq-default buffer-face-mode-face 'documents-face) ;; For documents buffers.
 	(set-face-attribute 'default nil
 						:height 160
 						:width 'normal
@@ -45,32 +38,45 @@
 						:inherit 'default
 						:slant 'italic))
 
+(defun joes-theme-apply-flymake()
+	"Flymake theme config."
+	(set-face-attribute 'flymake-error nil :underline '(:color "#F00" :style wave))
+	(set-face-attribute 'flymake-warning nil :underline '(:color "#FF0" :style wave))
+	(set-face-attribute 'flymake-note nil :underline '(:color "#00F" :style wave))
+	(setq-default flymake-note-bitmap '(right-arrow compilation-note))
+	(setq-default flymake-warning-bitmap '(right-triangle compilation-warning))
+	(setq-default flymake-error-bitmap '(flymake-double-exclamation-mark compilation-error)))
+
 (defun joes-theme-apply-zenburn()
+	"General colors based on zenburn."
 	(load-theme 'zenburn t)
 	(joes-theme-apply-default-faces)
 	(set-face-attribute 'default nil
 		:background "#111"
 		:foreground "#D6D0BC")
-	(set-face-attribute 'hl-line nil :background "#0A0A0A" :box '(:line-width -1 :color "#555"))
+	(set-face-attribute 'hl-line nil :background "#0D0D0D" :box '(:line-width -1 :color "#555"))
 	(set-face-attribute 'region nil :foreground 'unspecified :background "#18181F")
 	(set-face-attribute 'isearch nil :foreground 'unspecified)
 	(set-face-attribute 'lazy-highlight nil :foreground 'unspecified)
+	
+	(set-face-attribute 'mode-line nil :background "#161616" :height 140)
 
 	(set-face-attribute 'font-lock-keyword-face nil :foreground "#AA6" :weight 'bold)
 	(set-face-attribute 'font-lock-constant-face nil :inherit 'font-lock-keyword-face :foreground "#C66")
 	(set-face-attribute 'font-lock-builtin-face nil :foreground "#A5A5A5" :weight 'bold)
 
 	(set-face-attribute 'font-lock-type-face nil :foreground "#6292A8")
-	(set-face-attribute 'line-number nil :background "#151515")
+	(set-face-attribute 'line-number nil :background "#141414")
 	(set-face-attribute 'line-number-current-line nil :background "#111" :box '(:line-width -1 :color "#555"))
 
 	(set-face-attribute 'highlight nil :background 'unspecified :weight 'ultra-bold :underline "#FFF"))
 
 (defun joes-theme-apply-ivy()
+	"Ivy/Swiper/Counsel colors."
 	(require 'ivy-faces)
 	(set-face-attribute 'ivy-current-match nil
-		:background "#0A0A0A"
-		:box '(:line-width -1 :color "#555")
+		:background "#0A0A0E"
+		:box '(:line-width -1 :color "#606560")
 		:foreground 'unspecified
 		:underline 'unspecified
 		:extend t)
@@ -81,12 +87,14 @@
 	(set-face-attribute 'ivy-minibuffer-match-face-4 nil :background 'unspecified :underline "#FFF"))
 
 (defun joes-theme-apply-logview()
+	"Logview theme."
 	(set-face-attribute 'logview-warning-entry nil :foreground "#B90" :background 'unspecified)
 	(set-face-attribute 'logview-error-entry nil :foreground "#A33" :background 'unspecified)
 	(set-face-attribute 'logview-trace-entry nil :foreground "#AAC" :background 'unspecified)
 	(set-face-attribute 'logview-information-entry nil :foreground "#3B3" :background 'unspecified))
 
 (defun joes-theme-set-cursor()
+	"Mouse cursor."
 	(add-to-list 'default-frame-alist '(mouse-color . "#cca"))
 	(add-to-list 'default-frame-alist '(cursor-color . "#cca")))
 
