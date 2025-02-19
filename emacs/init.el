9085d5eb81cc5962f05fe0ed1526a5982997b295
Joe Kohler <joe.fbs.junior@gmail.com>
2025-02-19 18:39:06 -0300

    added license to init.el

--- emacs/init.el
+++ emacs/init.el
@@ -1,12 +1,29 @@
 ;;; Emacs --- Init file -*- lexical-binding: t; -*-
+;; Copyright (C) 2025  Joe Köhler
+
+;; Author: Joe Köhler <joe.fb.kohler@gmail.com>
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
 ;;; Commentary:
-;;; TODO:
-;;; dependency checker: aspell/hunspell
-;;; Check on Vertico.
-;;; remove company? counsel completion-at-point the convolluted
-;;; fix xref-apropos so it's useful. counsel, vertico, something!
-;;; Use tree-sitter whenever possible
-;;; --> Move elpa to ~/.local/share
+
+;; TODO:
+;; dependency checker: aspell/hunspell
+;; Check on Vertico.
+;; remove company? counsel completion-at-point the convolluted
+;; fix xref-apropos so it's useful.  counsel, vertico, something!
+;; Use tree-sitter whenever possible
+;; --> Move elpa to ~/.local/share
 
 ;;; Code:
 
